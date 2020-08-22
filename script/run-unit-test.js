80f017905e8662e0adc770a813cc7bfe070cfcf5
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-08-22 05:50:06 -0400

    Changed NYC reporters to be consistent with old coverage script reporters.

--- script/run-unit-test.js
+++ script/run-unit-test.js
@@ -37,7 +37,7 @@ if (options.help) {
 }
 
 const mochaPath = 'BABEL_ENV=test mocha';
-const coveragePath = `NODE_ENV=test nyc --all ${coverageInclude} --reporter=lcov --reporter=text-summary mocha --reporter mocha-junit-reporter --no-color`;
+const coveragePath = `NODE_ENV=test nyc --all ${coverageInclude} --reporter=lcov --reporter=text --reporter=json-summary mocha --reporter mocha-junit-reporter --no-color`;
 const testRunner = options.coverage ? coveragePath : mochaPath;
 const configFile = 'config/mocha.json';
 
