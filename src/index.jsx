3444f65dd102c57833cc9e9c7ae6afd50d62e52d
preetichavan <preeti.chavan@harbingergroup.com>
2020-09-08 16:31:14 +0530

    add scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import Hello from './hello';
-
+import './applications/proxy-rewrite/sass/style-consolidated.scss';
 // import "bootstrap/dist/css/bootstrap.css";
 // import PageNotFound from "./components/PageNotFound";
 // import Layout from "./components/layouts/Layout";
