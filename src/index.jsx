97082796de7218bd6bc59d8d93372d0fa6024264
preetichavan <preeti.chavan@harbingergroup.com>
2020-09-10 13:48:02 +0530

    remove comments and revert changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import Hello from './hello';
 import VetsCss from './applications/proxy-rewrite/sass/style-consolidated.scss';
-// import "bootstrap/dist/css/bootstrap.css";
-// import PageNotFound from "./components/PageNotFound";
-// import Layout from "./components/layouts/Layout";
-// export { HelloWorld, PageNotFound, Layout };
+
 import { RoutedSavablePage } from './platform/forms/save-in-progress/RoutedSavablePage';
 import { RoutedSavableReviewPage } from './platform/forms/save-in-progress/RoutedSavableReviewPage';
 export default { RoutedSavablePage, Hello, RoutedSavableReviewPage, VetsCss };
