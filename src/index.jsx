c3bde227b2b00746d2fdc52486f03668313eabae
Saumya Saxena <Saumya.Saxena@harbingergroup.com>
2020-09-10 18:27:29 +0530

    exported SavableApp

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import Hello from './hello';
 import VetsCss from './applications/proxy-rewrite/sass/style-consolidated.scss';
 
 import { RoutedSavablePage } from './platform/forms/save-in-progress/RoutedSavablePage';
+import { RoutedSavableApp } from './platform/forms/save-in-progress/RoutedSavableApp';
 import { RoutedSavableReviewPage } from './platform/forms/save-in-progress/RoutedSavableReviewPage';
 import {formConfig} from './applications/hca/config/form';
 
-export default { RoutedSavablePage, Hello, RoutedSavableReviewPage, VetsCss, formConfig };
+export default { RoutedSavablePage, Hello, RoutedSavableReviewPage, VetsCss, formConfig,RoutedSavableApp};
