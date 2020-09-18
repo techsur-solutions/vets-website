1984f86fd5778309d4edaa322b6f0dcfda24c3ce
Saumya Saxena <Saumya.Saxena@harbingergroup.com>
2020-09-18 17:13:11 +0530

    changes for routes

--- src/applications/hca/HealthCareApp.jsx
+++ src/applications/hca/HealthCareApp.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-//import formConfig from './config/form';
+import formConfig from './config/form';
 
-export default function HealthCareEntry( location, children,formConfig ) {
-  console.log("HealthCareEntry  location ************    ",location);
-  console.log("HealthCareEntry  children ************    ",children);
-  console.log("HealthCareEntry  formConfig ************    ",formConfig);
+export default function HealthCareEntry({ location, children }) {
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
     </RoutedSavableApp>
-   
   );
 }
--- src/applications/hca/routes.jsx
+++ src/applications/hca/routes.jsx
@@ -1,25 +1,15 @@
 import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/helpers';
-import {HealthCareEntry} from './HealthCareApp.jsx';
 
-// const routes = {
-//   path: '/',
-//   component: HealthCareApp,
-//   indexRoute: {
-//     onEnter: (nextState, replace) => replace('/introduction'),
-//   },
-//   childRoutes: createRoutesWithSaveInProgress(formConfig),
-// };
+import formConfig from './config/form';
+import HealthCareApp from './HealthCareApp.jsx';
 
-export function callRoutedApp(formConfig){
-  console.log("formConfig  ******* ",formConfig);
-  console.log("   ******************   ",createRoutesWithSaveInProgress(formConfig));
-  return HealthCareEntry("veteran-chap1/veteranInfo",createRoutesWithSaveInProgress(formConfig),formConfig);
-// const routes = {
-//   path: '/',
-//   component: HealthCareApp,
-//   indexRoute:,
-//   childRoutes: ,
-//   form: formConfig
-// };
-}
+const routes = {
+  path: '/',
+  component: HealthCareApp,
+  indexRoute: {
+    onEnter: (nextState, replace) => replace('/introduction'),
+  },
+  childRoutes: createRoutesWithSaveInProgress(formConfig),
+};
 
+export default routes;
