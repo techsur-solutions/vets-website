52c86296b81076ff1281624898ca5065245dedb6
preetichavan <preeti.chavan@harbingergroup.com>
2020-09-22 14:43:01 +0530

    add build

--- src/platform/forms/save-in-progress/RoutedSavablePage.jsx
+++ src/platform/forms/save-in-progress/RoutedSavablePage.jsx
@@ -88,13 +88,13 @@ class RoutedSavablePage extends React.Component {
 
 function mapStateToProps(state, ownProps) {
   console.log('ownProps +++++++ ', ownProps.route);
-  const { appStateSelector } = ownProps.route.pageConfig;
+  // const { appStateSelector } = ownProps.route.pageConfig;
   return {
     form: state.form,
     user: state.user,
     showLoginModal: state.navigation.showLoginModal,
-    appStateData: null,
-    formConfig: ownProps.route.formConfig,
+    // appStateData: null,
+    // formConfig: ownProps.route.formConfig,
   };
 }
 
